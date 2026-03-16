01b47d25707436219c12028d7042e4e09c0b0b35
valdir1985 <valdir1985@gmail.com>
2026-03-16 20:41:48 -0300

    Update main.py

--- backend/main.py
+++ backend/main.py
@@ -2,9 +2,11 @@ from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from backend.config import settings
 from backend.database import engine, Base
-from backend.routers import auth, clinicas, usuarios # <-- Nova importação aqui
-from backend.models import convite # <-- Importar para o SQLAlchemy criar a tabela
+# Importamos todos os routers construídos até agora
+from backend.routers import auth, clinicas, usuarios, pacientes 
+from backend.models import convite, paciente # Importar os models para criar as tabelas
 
+# Cria as tabelas na base de dados
 Base.metadata.create_all(bind=engine)
 
 app = FastAPI(
@@ -21,10 +23,11 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
-# Registro das rotas
+# Registo das rotas (Endpoints)
 app.include_router(auth.router, prefix=f"{settings.API_V1_STR}/auth", tags=["Autenticação"])
 app.include_router(clinicas.router, prefix=f"{settings.API_V1_STR}/clinicas", tags=["Clínicas"])
-app.include_router(usuarios.router, prefix=f"{settings.API_V1_STR}/usuarios", tags=["Usuários"]) # <-- Novo router
+app.include_router(usuarios.router, prefix=f"{settings.API_V1_STR}/usuarios", tags=["Usuários"])
+app.include_router(pacientes.router, prefix=f"{settings.API_V1_STR}/pacientes", tags=["Pacientes"]) # <-- NOVA ROTA
 
 @app.get("/")
 def root():
